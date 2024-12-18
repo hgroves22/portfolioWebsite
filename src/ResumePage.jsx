664c78eec74454d344e50387fb902272265ba4fd
hgroves22 <henry.groves@richmond.edu>
2024-12-18 18:32:45 -0500

    changed fade in hero / fixed bc in resume

--- src/ResumePage.jsx
+++ src/ResumePage.jsx
@@ -6,24 +6,24 @@ import './components/ResumePage.css'
 function Resume() {
 
   return(
-    <div className="resume-container">
-      <div className="education-container">
-        <h2 className="sub-header">EDUCATION</h2>
-        <p className="resume-info">something something seomthing</p>
-        <p className="resume-info">something something seomthing</p>
-        <br></br>
-      </div>
-      <div className="experience-container">
-        <h2 className="sub-header">PROFESSIONAL</h2>
-        <h2 className="sub-header">EXPERIENCE</h2>
-        <p className="resume-info">something something seomthing</p>
-        <br></br>
-      </div>
-      <div className="technical-container">
-        <h2 className="sub-header">TECHNICAL</h2>
-        <h2 className="sub-header">SKILLS</h2>
-        <p className="resume-info">something something seomthing</p>
-        <br></br>
+    <div className='page'>
+      <div className="resume-container">
+        <div className="education-container">
+          <h2 className="sub-header">EDUCATION</h2>
+          <br></br>
+          <p className="resume-info">something something seomthing</p>
+          <p className="resume-info">something something seomthing</p>
+        </div>
+        <div className="experience-container">
+          <h2 className="sub-header">PROFESSIONAL</h2>
+          <h2 className="sub-header">EXPERIENCE</h2>
+          <p className="resume-info">something something seomthing</p>
+        </div>
+        <div className="technical-container">
+          <h2 className="sub-header">TECHNICAL</h2>
+          <h2 className="sub-header">SKILLS</h2>
+          <p className="resume-info">something something seomthing</p>
+        </div>
       </div>
     </div>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,30 +14,30 @@ function Hero() {
         <div className="hero-content">
           <h1 className="title fade-in1">Hi, I'm Henry!</h1>
           <h1 className='fade-in2'>A recent graduate and an upcoming developer</h1>
-          <h3 className='fade-in2'>I love work on projects and excited to bring the love into the professional world</h3>
+          <p className='fade-in2'>I love to work on projects and excited to bring the love into the professional world</p>
 
-          <p className="socials-desc fade-in3">Connect with  me on LinkedIn or come take a look at my GitHub repo!</p>
+          <p className="socials-desc">Connect with  me on LinkedIn or come take a look at my GitHub repo!</p>
 
           <div className="social-medias">
             <a 
               href="https://www.linkedin.com/in/henry-groves-540ba1229/" 
               target="_blank"
               rel="noopener noreferrer"
-              className="linkedin-link fade-in3">
+              className="linkedin-link">
             <FaLinkedin size={70} style={{ color: "#BDC3C7" }} />
             </a>
             <a 
               href="https://github.com/hgroves22?tab=repositories" 
               target="_blank"
               rel="noopener noreferrer"
-              className="github-link fade-in3">
+              className="github-link">
             <FaGithub size={70} style={{ color: "#BDC3C7" }} />
             </a>
           </div>
         </div>
 
         <div className="hero-picture">
-          <img className="headshot fade-in3" src= {headshot} alt="This should be a photo of me!" />
+          <img className="headshot" src= {headshot} alt="This should be a photo of me!" />
         </div>
 
       </section>
