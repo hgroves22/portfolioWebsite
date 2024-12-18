6bcfe7760fc92e2ee54260317842fb2801532b83
hgroves22 <henry.groves@richmond.edu>
2024-12-18 15:06:32 -0500

    Initial Resume Page

--- src/ResumePage.jsx
+++ src/ResumePage.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
+import './components/ResumePage.css'
+
+
 
 function Resume() {
 
   return(
-    <div className="resumeContainer">
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <h1>THIS IS Resume</h1>
+    <div className="resume-container">
+      <div className="education-container">
+        <h2 className="sub-header">EDUCATION</h2>
+        <p className="resume-info">something something seomthing</p>
+        <p className="resume-info">something something seomthing</p>
+        <br></br>
+      </div>
+      <div className="experience-container">
+        <h2 className="sub-header">PROFESSIONAL</h2>
+        <h2 className="sub-header">EXPERIENCE</h2>
+        <p className="resume-info">something something seomthing</p>
+        <br></br>
+      </div>
+      <div className="technical-container">
+        <h2 className="sub-header">TECHNICAL</h2>
+        <h2 className="sub-header">SKILLS</h2>
+        <p className="resume-info">something something seomthing</p>
+        <br></br>
+      </div>
     </div>
   );
 }
