8409601646e8300ff79d9a9b720042c4bb184f4e
hgroves22 <henry.groves@richmond.edu>
2024-12-30 16:56:48 -0500

    Add professional experience to resume page

--- src/ResumePage.jsx
+++ src/ResumePage.jsx
@@ -47,8 +47,32 @@ function Resume() {
         <div className="experience-container fade-in2">
           <h2 className="sub-header">PROFESSIONAL</h2>
           <h2 className="sub-header header-pad">EXPERIENCE</h2>
-          <h3 className="resume-info">Independent Software Consultant</h3>
-          <h3 className="resume-info">Financial Analyst</h3>
+
+          <p className="resume-info">
+            <strong>Independant Software Consultant</strong>
+            <p className='list-desc'><strong>Date: </strong>November '24 - Present</p>
+            <p className='list-desc'><strong>Location: </strong>Remote</p>
+            <p className='list-desc'><strong>Key Responsibilities: </strong></p>
+            <ul className="list-desc-list">
+              <li>Established strong relationships with three clients to provide support on innovative software projects.</li>
+              <li>Debug and optimize a projection project in Python and TypeScript for an upcoming performance in 2025.</li>
+              <li>Customize and improve a startup’s website design using the React library, enhancing the user interface.</li>
+            </ul>
+          </p>
+            
+
+          <p className="resume-info">
+            <strong>Financial Analyst Intern</strong>
+            <p className='list-desc'><strong>Date: </strong>June '23 - August '23</p>
+            <p className='list-desc'><strong>Company: </strong>Gabelli Funds, LLC</p>
+            <p className='list-desc'><strong>Location: </strong>Rye, NY</p>
+            <p className='list-desc'><strong>Key Responsibilities: </strong></p>
+            <ul className="list-desc-list">
+              <li>Worked in the merger arbitrage division researching and analyzing new acquisitions within companies.</li>
+              <li>Participated in earnings calls with research analysts and shadowed traders on the in-house trading floor.</li>
+              <li>Developed a financial model and stock pitch for YETI; presented to all research analysts and CEO of firm.</li>
+            </ul>
+          </p>
         </div>
         <div className="technical-container fade-in3">
           <h2 className="sub-header">TECHNICAL</h2>
